c3d40a28d916cbb20a8e851827643f95a8da8f55
Mariano Tucat <mariano@skedgo.com>
2018-04-18 23:59:38 -0300

    Sals pizza v4

---
+++ public/javascripts/controladorPedido.js
@@ -0,0 +1,53 @@
+var pedido;
+
+function ordenarPizzas(data) {
+    var index;
+
+    var pizzas = new Object();
+    for (index = 0; index < data.length; ++index) {
+        var pizza = data[index];
+        pizza.id = index;
+        pizza.enPedido = false;
+        var pizzasPorTipo;
+        if (pizzas.hasOwnProperty(pizza.tipo))
+            pizzasPorTipo = pizzas[pizza.tipo];
+        else {
+            pizzasPorTipo = new Array();
+            pizzas[pizza.tipo] = pizzasPorTipo;
+        }
+        pizzasPorTipo[pizzasPorTipo.length] = pizza;
+    }
+    return pizzas;
+}
+
+function actualizarPedido(e) {
+    var target = e.target;
+    while (target.tagName !== "TR") {
+        target = target.parentNode;
+    }
+    var id = target.getAttribute("id");
+    
+    var pizza = pedido[id];
+    pizza.enPedido = !pizza.enPedido;
+
+    //le cambio el estado
+    actualizarEstado(id, pizza.enPedido);
+
+    //recalculo el total
+    actualizarTotal(computarTotal());
+}
+
+/**
+ * Calcula el valor del pedido
+ */
+function computarTotal() {
+    var total = 0.0;
+
+    for (index = 0; index < pedido.length; ++index) {
+        var pizza = pedido[index];
+        if (pizza.enPedido) {
+            total += pizza.precio;
+        }
+    }
+    return total;
+}
